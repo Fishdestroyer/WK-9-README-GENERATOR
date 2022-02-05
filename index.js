3f0402099e76cf85fba0ca52dc3baa11aabb32f8
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-05 01:20:25 -0600

    Added markdown sections and license badges (hopefully they work)'

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown.js');
+
 
 const promptUser =() => {
     return inquirer.prompt ([
+        //project title
         {
             type: 'input',
-            name: 'projectName',
-            message: 'What is the name of your project?'
+            name: 'projectTitle',
+            message: 'What is your projects title?'
         },
+        //Description of project
         {
             type: 'input',
             name: 'description',
             message: 'Enter a description of your project'
         },
+        // Table of contents section
+        {
+            type: 'input',
+            name: 'table',
+            message: 'Please list the table of contents'
+        },
+        //Required installation
         {
         type: 'input',
         name: 'installation',
         message: 'What do you need to install to make this project run correctly?'
 
         },
+        //usage section
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How will this project be used?'
+        },
+        //License option
         {
             type: 'checkbox',
             name: 'license',
             message: 'what license woulde you like to use?',
             choices: ['MIT','GNU', 'Apache']
         },
+        //additional resources and mentions
         {
             type: 'input',
-            name: 'technologies',
-            message: 'List any additional technologies used for this project'
+            name: 'additional resources',
+            message: 'List any additional resources or contributors that assisted in this project.'
         },
+        //Github Link
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter your GitHub link here'
+        }
 
     ]);
 };
